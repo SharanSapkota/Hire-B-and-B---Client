aea539bce538ae9d2155dfc9a9b5657dd47347ed
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-10-17 15:53:26 +0300

    revamp code into src folder

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,9 +1,12 @@
 import { UserSignupPayload } from "@/interface/user-interface";
+import axios from "axios";
 
 export const signUp = async (payload: UserSignupPayload) => {
     const {firstName, secondName, lastName, phone, dob, email, password, role } = payload;
+
     try {
-        const response: any = await axios.post(`${process.env.API_BASE_URL || "http://localhost:4000/api"}/signup`, {
+        console.log("API_BASE_URL:", process.env);
+        const response: any = await axios.post(`${process.env.API_BASE_URL + '/auth' || "http://localhost:4000/api/auth"}/signup`, {
             firstName,
             secondName,
             lastName,
